691f5f1030367dc70cd88732158f07b9ee0c2039
Chong Zhi Hong <zhihong.chong@bertelsmann.de>
2024-07-09 16:38:14 +0800

    Update

--- Day 1/src/components/CoreConcept.jsx
+++
@@ -1,9 +0,0 @@
-export function CoreConcept({ image, title, description }) {
-  return (
-    <li>
-      <img src={image} alt="alt text" />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
